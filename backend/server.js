757b6e66d6417cb81152b8cfba4e62e66d757106
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2026-03-17 11:42:14 +0530

    feat(backend): add mongoose connection, db config, and user model

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const url = process.env.MONGODB_URI;
 
 app.get("/", (req, res) => {
   res.json({ success: true, message: "API Running" });
 });
+
+connectDB(url).then(() => console.log("MongoDB Connected!"));
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
